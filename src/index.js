1d5a661eb3b43330039a46be7cb994d10fa2180d
svarkhinos <svitlana.arkhinos@gmail.com>
2021-06-12 16:04:46 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
 import './css/main.css';
+import ApiService from './js/api-service';
+import picturesTpl from './templates/pictures.hbs';
+import LoadMoreBtn from './js/load-moreBtn';
+import { alert, defaults } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+
+defaults.delay = '2000';
+
+const refs = {
+    searchForm: document.querySelector('.js-search-form'),
+    galleryContainer: document.querySelector('.gallery'),
+};
+
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+});
+
+const apiService = new ApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchPictures);
+
+function onSearch(e) {
+    e.preventDefault();
+
+    apiService.query = e.currentTarget.elements.query.value;
+
+    if (apiService.query === '') {
+        return alert('Enter you request');
+    }
+
+    loadMoreBtn.show();
+    apiService.resetPage();
+    clearPicturesContainer();
+    fetchPictures();
+}
+
+function fetchPictures() {
+    loadMoreBtn.disable();
+
+    apiService.fetchPictures().then(hits => {
+        appendPicturesMarkup(hits);
+        loadMoreBtn.enable();
+    });
+}
+
+function appendPicturesMarkup(hits) {
+    refs.galleryContainer.insertAdjacentHTML('beforeend', picturesTpl(hits));
+    autoScroll();
+}
+
+function clearPicturesContainer() {
+    refs.galleryContainer.innerHTML = '';
+}
+
+function autoScroll() {
+    loadMoreBtn.refs.button.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+    });
+}
---
+++ src/js/api-service.js
@@ -0,0 +1,37 @@
+const API_KEY ='21915406-973315f608c7e35a5d0268cdc'
+
+export default class ApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+
+    fetchPictures() {
+
+        
+      return  fetch(
+        `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`,
+    )
+        .then(response => response.json())
+        .then(({hits}) =>{
+            this.incrementPage();
+            return hits;
+        });
+    }
+
+incrementPage() {
+    this.page +=1;
+}
+
+ resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query (newQuery) {
+        this.searchQuery = newQuery
+    }
+}
